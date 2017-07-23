5f4af471947d933251e5d349ed961bc4e8b58cec
Paulo Cesar Ferreira de Mello <paulo@javascript.floripa.br>
2017-07-23 18:52:45 -0300

    Prototype inheritance
    The prototypes inheritace examples has been created.

--- animal.js
+++ animal.js
@@ -1,20 +1,11 @@
-var Animal = function() {
+function Animal(name, type) {
+    this.name = name;
+    this.type = type;
+}
 
-};
-
-Animal.prototype.name = 'No name';
-Animal.prototype.type = 'No type';
 Animal.prototype.say = function() {
     console.log('I\'m saying');
 };
 Animal.prototype.move = function() {
     console.log('I\'m moving');
 };
-
-console.log('Animal Object', new Animal());
-var cat = new Animal();
-cat.name = 'Joe';
-cat.type = 'Cat';
-cat.say();
-cat.move();
-console.log('Cat Animal', cat);
